446c685f4dbfd728865eb1aa7993f9e661352f6d
Peter <schmidt.multimedia@gmail.com>
2023-02-21 11:06:50 +0100

    02_Kardinalität 1:1

--- 01_FirstProject/sql/02_users.sql
+++ 01_FirstProject/sql/02_users.sql
@@ -14,7 +14,7 @@ DESCRIBE mydb.users;
 -- Inserts
 INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "max", DEFAULT);
 INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "maxine", DEFAULT);
-INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "max", DEFAULT);
+INSERT INTO `mydb`.`users` (`id`, `username`, `create_time`) VALUES (DEFAULT, "maxl", DEFAULT);
 
 -- Inhalte
 SELECT * FROM mydb.users;
---
+++ 02_kardinal_1-1/sql/01_tabs_unverbunden.sql
@@ -0,0 +1,21 @@
+-- Tabellen, unverbunden
+
+DROP TABLE IF EXISTS mydb.cats;
+DROP TABLE IF EXISTS mydb.servants;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+DESCRIBE mydb.cats;
+DESCRIBE mydb.servants;

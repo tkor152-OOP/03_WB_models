dfe449ad94085b64f62a6b67752bb6ae60e77bb5
git4cc-noob <122432488+git4cc-noob@users.noreply.github.com>
2023-02-21 12:13:43 +0100

    Update SQL UNIQUE

---
+++ 02_Kardinalitäten/_chen/04_tabs_w_inserts_unique.sql
@@ -0,0 +1,50 @@
+-- Tabellen mit Dateninserts
+-- Vorbereitungen
+DROP TABLE IF EXISTS mydb.servants;
+DROP TABLE IF EXISTS mydb.cats;
+
+-- Mastertabelle
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- Struktur: MT
+DESCRIBe mydb.cats;
+
+-- Inhalte: MT
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Grizabella", "white");
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Alonzo", "grey");
+INSERT INTO `mydb`.`cats` (`id`, `cat_name`, `fur_color`) VALUES (DEFAULT, "Mausi", "striped");
+
+-- Inhalte: MT
+SELECT * FROM mydb.cats;
+
+-- Detailtabelle (DT) mit Verbindung zur Mastertabelle mit Fremdschlüssel (FK)
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  `cats_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC),
+  UNIQUE INDEX `cats_id_UNIQUE` (`cats_id` ASC),
+  CONSTRAINT `fk_servants_cats`
+    FOREIGN KEY (`cats_id`)
+    REFERENCES `mydb`.`cats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- Struktur: DT
+DESCRIBE mydb.servants;
+
+-- Inhalte: Detailtabelle
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Peter", 5, 3);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Patrick", 3, 1);
+INSERT INTO `mydb`.`servants` (`id`, `servant_name`, `yrs_served`, `cats_id`) VALUES (DEFAULT, "Daniel", 4, 2);
+
+-- Inhalte: DT
+SELECT * FROM mydb.servants;
